1bb283fb5721d569eb2713c4740da7990be4f001
Dorian <doriandev2020@gmail.com>
2020-11-28 08:45:27 -0600

    Modified the User model to include a first and last name

---
+++ src/main/java/com/example/auth/Post.java
@@ -0,0 +1,8 @@
+package com.example.auth;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Post {
+
+}
--- src/main/java/com/example/auth/User.java
+++ src/main/java/com/example/auth/User.java
@@ -10,29 +10,56 @@ import javax.persistence.Id;
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+  private int ID;
   
-  @Column(nullable = false, unique = true)
+  public int getID() {
+	return ID;
+}
+
+public void setID(int iD) {
+	ID = iD;
+}
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+@Column(nullable = false, unique = true)
   private String username;
   
   private String password;
   
-  public Long getId() {
-    return id;
-  }
-  public void setId(Long id) {
-    this.id = id;
-  }
-  public String getUsername() {
-    return username;
-  }
-  public void setUsername(String username) {
-    this.username = username;
-  }
-  public String getPassword() {
-    return password;
-  }
-  public void setPassword(String password) {
-    this.password = password;
-  }
+  private String firstName;
+  
+  private String lastName;
+  
+  
 }
