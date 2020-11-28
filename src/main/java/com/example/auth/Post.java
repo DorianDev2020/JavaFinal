c46ff7111e4a58fce2abece0aed390ba72cac98e
Dorian <doriandev2020@gmail.com>
2020-11-28 08:49:59 -0600

    Created the Post Model class

--- src/main/java/com/example/auth/Post.java
+++ src/main/java/com/example/auth/Post.java
@@ -1,8 +1,46 @@
 package com.example.auth;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Post {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	  private int ID;
+	
+	@Column(nullable = false, unique = true)
+	
+	private String authorID;
+	private String message;
+	private LocalDateTime timeStamp;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getAuthorID() {
+		return authorID;
+	}
+	public void setAuthorID(String authorID) {
+		this.authorID = authorID;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public LocalDateTime getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(LocalDateTime timeStamp) {
+		this.timeStamp = timeStamp;
+	}
 }
